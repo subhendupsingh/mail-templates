d5506f7196764304b0eae141e9bed215a919ae59
Subhendu Pratap Singh <sps.1431990@gmail.com>
2023-11-02 15:02:33 +0530

    Add files via upload

--- test.precompiled.js
+++ test.precompiled.js
@@ -1,4 +1,5 @@
-var Handlebars = require("handlebars/runtime");  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['test'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
@@ -7,7 +8,8 @@ templates['test'] = template({"compiler":[8,">= 4.3.0"],"main":function(containe
         return undefined
     };
 
-  return "Handlebars <b>"
-    + container.escapeExpression(((helper = (helper = lookupProperty(helpers,"doesWhat") || (depth0 != null ? lookupProperty(depth0,"doesWhat") : depth0)) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"doesWhat","hash":{},"data":data,"loc":{"start":{"line":1,"column":14},"end":{"line":1,"column":26}}}) : helper)))
-    + "</b> precompiled!";
+  return "<script id=\"handlebars-template\" type=\"text/x-handlebars-template\">\n    Handlebars <b>"
+    + container.escapeExpression(((helper = (helper = lookupProperty(helpers,"doesWhat") || (depth0 != null ? lookupProperty(depth0,"doesWhat") : depth0)) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"doesWhat","hash":{},"data":data,"loc":{"start":{"line":2,"column":18},"end":{"line":2,"column":30}}}) : helper)))
+    + "</b> precompiled!\n</script>";
 },"useData":true});
+})();
