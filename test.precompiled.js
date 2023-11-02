fc8a88369f28b8c626ce82d3321531693b00b58b
Subhendu Pratap Singh <sps.1431990@gmail.com>
2023-11-02 14:45:14 +0530

    Add files via upload

--- test.precompiled.js
+++ test.precompiled.js
@@ -1,5 +1,4 @@
-(function() {
-  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+var Handlebars = require("handlebars/runtime");  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['test'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
@@ -12,4 +11,3 @@ templates['test'] = template({"compiler":[8,">= 4.3.0"],"main":function(containe
     + container.escapeExpression(((helper = (helper = lookupProperty(helpers,"doesWhat") || (depth0 != null ? lookupProperty(depth0,"doesWhat") : depth0)) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"doesWhat","hash":{},"data":data,"loc":{"start":{"line":1,"column":14},"end":{"line":1,"column":26}}}) : helper)))
     + "</b> precompiled!";
 },"useData":true});
-})();
